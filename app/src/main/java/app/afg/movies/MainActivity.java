0b2fc2dac9c1955fb2ec1ea658691a4d76345cef
Devcode5 <55508590+Devcode5@users.noreply.github.com>
2021-06-11 18:04:30 +0430

    Initial commit

---
+++ app/src/main/java/app/afg/movies/MainActivity.java
@@ -0,0 +1,16 @@
+package app.afg.movies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //https://run.mocky.io/v3/d921465b-b870-4ce3-b2a5-17a4b7621f96
+    private static String JSON_URL = "https://run.mocky.io/v3/d921465b-b870-4ce3-b2a5-17a4b7621f96";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
